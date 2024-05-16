d74f846ba9e8ef94e62c972e0a0a8c0f8c6f2194
barnapet <barnapet00@gmail.com>
2024-05-16 10:37:56 +0200

    Offline fejlesztés feltöltése

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Csúcs startCsúcs;
+        GráfKereső kereső;
+        Console.WriteLine("A 3 szerzetes 3 kanibál problémát oldjuk meg.");
+        startCsúcs = new Csúcs(new SzerzetesekÉsKannibálokÁllapot(3, 3));
+        Console.WriteLine("A kereső egy 15 mélységi korlátos és emlékezetes backtrack.");
+        kereső = new BackTrack(startCsúcs, 15, true);
+        kereső.megoldásKiírása(kereső.Keresés());
+        Console.WriteLine("A megoldáshoz elegendő: " + kereső.Keresés().GetMélység()
+                          + " mélységi korlát is.");
+        System.Console.WriteLine();
+        Console.WriteLine("A kereső egy mélységi keresés körfigyeléssel.");
+        kereső = new MélységiKeresés(startCsúcs, true);
+        kereső.megoldásKiírása(kereső.Keresés());
+        Console.ReadLine();
+    }
+}
